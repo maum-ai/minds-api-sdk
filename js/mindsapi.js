e4ffe76902796692f5bc55c655bbc486ee22ca3e
golch <golch@mindslab.ai>
2017-09-14 18:15:08 +0900

    STT javascript 테스트 코드 작성
    nodejs 및 webpage(html) 환경에서 테스트 할 수 있는 테스트 코드 작성

---
+++ js/mindsapi.js
@@ -0,0 +1,42 @@
+//POST
+var request = require('request');
+var fs = require('fs');
+var FormData = require('form-data');
+
+// 헤더 부분
+var headers = {
+    'User-Agent':   'Super Agent/0.0.1',
+    'enctype-type': 'multipart/form-data'
+};
+
+// 요청 세부 내용
+var options = {
+    url: 'http://127.0.0.1:8000/api/stt/',
+    method:'POST',
+    encoding:'utf-8',
+    headers: headers,
+    formData:{
+        'ID': 'minds-api-service-client-id',
+        'key': 'minds-api-service-client-key',
+        'lang': 'kor',
+        'level': 'baseline',
+        'sampling': 8000,
+        'cmd': 'runFileStt',
+        'file': [fs.createReadStream('../audio/hello-8k.wav')]
+    }
+};
+
+// 요청 시작 받은값은 body
+var req = request(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(unicodeToChar(body).replace(/\\n/g, ". "));
+    }
+});
+
+
+function unicodeToChar(text) {
+    return text.replace(/\\u[\dA-F]{4}/gi,
+        function (match) {
+            return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+        });
+}
