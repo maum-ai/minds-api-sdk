431737faf86f0041f58423389534c6739ad5e575
golch <golch@mindslab.ai>
2019-01-07 15:09:12 +0900

    API URL changed

--- py/mindsapi_env.py
+++ py/mindsapi_env.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 """MindsAPI environment
 """
-API_FRONT_URL = "https://mindsapi.mindslab.ai/api/"
 
-# API_FRONT_URL = "http://127.0.0.1:8000/api/"
+API_FRONT_URL = "https://api.maum.ai/api/"
+
